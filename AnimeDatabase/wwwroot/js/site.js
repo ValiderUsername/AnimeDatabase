7bf262352ce134115270f64f02ae5bf8f969f9ae
TillLS <till@kauz.ch>
2022-05-18 16:10:13 +0200

    Navbar responsive

--- AnimeDatabase/wwwroot/js/site.js
+++ AnimeDatabase/wwwroot/js/site.js
@@ -40,3 +40,13 @@ function showTime() {
 }
 
 showTime();
+
+
+function MyNavbar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
